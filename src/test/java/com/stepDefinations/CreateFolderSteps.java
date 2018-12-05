c994cf2302d61b14829796de438e003fb8aa0d5f
adewan1781 <adewan1781@gmail.com>
2018-12-05 09:25:16 +0530

    amit changes@2

--- src/test/java/com/stepDefinations/CreateFolderSteps.java
+++ src/test/java/com/stepDefinations/CreateFolderSteps.java
@@ -31,7 +31,7 @@ public class CreateFolderSteps {
 		wait.until(ExpectedConditions.attributeToBe(By.cssSelector("div[class=\"action-bar-create-buttons\"]"), "data-resin-feature", "createbuttons"));
 	    WebElement newoption= wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[aria-owns=\"menu-newdropdown\"]")));
          newoption.click();
-         System.out.println("Demo-Nipun123");
+         System.out.println("Demo-Nipun12345");
 	}
 
 	@And("^click on new folder link$")
